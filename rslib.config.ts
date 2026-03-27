21a044f5743b83dc54e443a8e568fc697a1219cf
C. Spencer Beggs <spencer@savvyweb.systems>
2026-03-27 19:59:11 -0400

    chore: update rslib deletions
    Signed-off-by: C. Spencer Beggs <spencer@savvyweb.systems>

--- rslib.config.ts
+++ rslib.config.ts
@@ -3,9 +3,12 @@ import { NodeLibraryBuilder } from "@savvy-web/rslib-builder";
 export default NodeLibraryBuilder.create({
 	transform({ pkg }) {
 		delete pkg.devDependencies;
-		delete pkg.scripts;
+		delete pkg.bundleDependencies;
 		delete pkg.publishConfig;
+		delete pkg.packageManager;
 		delete pkg.devEngines;
+		delete pkg.config;
+		delete pkg.scripts;
 		return pkg;
 	},
 });
